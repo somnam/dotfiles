fc26e3d87e565ef8a75f7fd750911e394a2f14f6
Tomek Miodek <tomek.miodek@gmail.com>
2016-03-11 19:11:12 +0100

    Restore keyboard brightness level on hibernation

--- lib/systemd/system-sleep/fix_wakeup.sh
+++ lib/systemd/system-sleep/fix_wakeup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 case $1/$2 in
-    pre/*)
+    pre/suspend)
+        # Don't wake up immediately when suspending.
         for device in 'EHC1' 'EHC2' 'XHC'; do
             state=`cat /proc/acpi/wakeup | grep "\b$device\b" | awk '{print $3}' | tr -d '*'`
             if [[ "$state" == "enabled" ]]; then
@@ -9,6 +10,13 @@ case $1/$2 in
             fi
         done
         ;;
-    post/*)
+    post/hibernate)
+        # Restore keyboard brightness level on hibernation.
+        value=$(cat /var/lib/systemd/backlight/platform-asus-nb-wmi\:leds\:asus\:\:kbd_backlight)
+        dbus-send --type=method_call --print-reply=literal --system       \
+            --dest='org.freedesktop.UPower'                               \
+            '/org/freedesktop/UPower/KbdBacklight'                        \
+            'org.freedesktop.UPower.KbdBacklight.SetBrightness'           \
+            "int32:${value}}"
         ;;
 esac
