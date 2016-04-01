bd65c19003daf5cb6530a219787c9edcabab3c22
Tomek Miodek <tomek.miodek@gmail.com>
2016-04-01 21:31:27 +0200

    Minor updates

--- home/tomek/.config/bashrc.d/exports.bash
+++ home/tomek/.config/bashrc.d/exports.bash
@@ -1,20 +1,6 @@
 # Editor
 export EDITOR='vim'
 
-# Colored GCC warnings and errors
-# export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
-
-# Android
-# export ANDROID_HOME=/home/tomek/.local/opt/android-sdk-linux
-# export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/platform-tools
-
-# Python
-export PYTHONPATH=$PYTHONPATH:/home/tomek/.local/lib/python2.7/site-packages
-export PYTHONSTARTUP=/home/tomek/.pystartup
-
-# Nodejs
-export NODE_PATH=$NODE_PATH:$(npm root -g)
-
 # Git
 export GIT_PS1_SHOWDIRTYSTATE='y'
 export GIT_PS1_SHOWSTASHSTATE='y'
@@ -23,4 +9,4 @@ export GIT_PS1_DESCRIBE_STYLE='contains'
 export GIT_PS1_SHOWUPSTREAM='auto'
 
 # Automatically trim long paths in the prompt (requires Bash 4.x)
-PROMPT_DIRTRIM=4
+PROMPT_DIRTRIM=3
