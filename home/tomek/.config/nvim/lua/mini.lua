d9cd71bec23a10aa51a5a9ccda8bb5b3068f398c
Tomek Miodek <tomek.miodek@gmail.com>
2022-08-03 20:13:43 +0200

    Neovim switch to lua config and plugins

--- home/tomek/.config/nvim/lua/mini.lua
+++ home/tomek/.config/nvim/lua/mini.lua
@@ -14,15 +14,15 @@ require('mini.cursorword').setup({
 
 require('mini.pairs').setup({
   mappings = {
-    ['('] = { neigh_pattern = '.[^%w\\]' },
-    [')'] = { neigh_pattern = '.[^%w\\]' },
-    ['['] = { neigh_pattern = '.[^%w\\]' },
-    [']'] = { neigh_pattern = '.[^%w\\]' },
-    ['{'] = { neigh_pattern = '.[^%w\\]' },
-    ['}'] = { neigh_pattern = '.[^%w\\]' },
-    ['"'] = { neigh_pattern = '.[^%w\\]' },
-    ["'"] = { neigh_pattern = '.[^%w\\]' },
-    ['`'] = { neigh_pattern = '.[^%w\\]', },
+    ['('] = { neigh_pattern = '.[^%w]' },
+    [')'] = { neigh_pattern = '.[^%w]' },
+    ['['] = { neigh_pattern = '.[^%w]' },
+    [']'] = { neigh_pattern = '.[^%w]' },
+    ['{'] = { neigh_pattern = '.[^%w]' },
+    ['}'] = { neigh_pattern = '.[^%w]' },
+    ['"'] = { neigh_pattern = '.[^%w]' },
+    ["'"] = { neigh_pattern = '.[^%w]' },
+    ['`'] = { neigh_pattern = '.[^%w]' },
   }
 })
 
@@ -31,16 +31,27 @@ require('mini.sessions').setup({
 })
 
 local starter = require('mini.starter')
+local actions_section = function()
+  return function()
+    return {
+      { action = 'Telescope oldfiles',        name = 'Previous files',  section = 'Actions' },
+      { action = 'Telescope find_files',      name = 'Find files',      section = 'Actions' },
+      { action = 'Telescope git_files',       name = 'Git files',       section = 'Actions' },
+      { action = 'Telescope live_grep',       name = 'Live grep',       section = 'Actions' },
+      { action = 'Telescope command_history', name = 'Command history', section = 'Actions' },
+      { action = 'enew',                      name = 'New buffer',      section = 'Actions' },
+      { action = 'qall',                      name = 'Quit Neovim',     section = 'Actions' },
+    }
+  end
+end
 starter.setup({
   evaluate_single = true,
   items = {
-    starter.sections.builtin_actions(),
-    starter.sections.recent_files(15, false),
-    starter.sections.sessions(5, true)
+    actions_section(),
   },
   content_hooks = {
     starter.gen_hook.adding_bullet(),
-    starter.gen_hook.indexing('all', { 'Builtin actions' }),
+    starter.gen_hook.indexing('all', { 'Actions' }),
     starter.gen_hook.padding(3, 2),
   },
 })
