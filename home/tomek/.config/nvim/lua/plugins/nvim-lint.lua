3d47590149663db9324f0a366d04fb5dfdaf07a8
Tomek Miodek <tomek.miodek@gmail.com>
2022-09-04 19:54:19 +0200

    Nvim updates

--- home/tomek/.config/nvim/lua/plugins/nvim-lint.lua
+++ home/tomek/.config/nvim/lua/plugins/nvim-lint.lua
@@ -20,13 +20,15 @@ end
 -- Python linters list.
 local python_linters = {}
 
-local python_flake_cmd = vim.fn.stdpath("data") .. "/python/bin/flake8"
+local vim_data_path = vim.fn.stdpath("data")
+
+local python_flake_cmd = vim_data_path .. "/python/bin/flake8"
 if vim.fn.executable(python_flake_cmd) == 1 then
   table.insert(python_linters, 'flake8')
   lint.linters.flake8.cmd = python_flake_cmd
 end
 
-local python_mypy_cmd = vim.fn.stdpath("data") .. "/python/bin/mypy"
+local python_mypy_cmd = vim_data_path .. "/python/bin/mypy"
 if vim.fn.executable(python_mypy_cmd) == 1 then
   table.insert(python_linters, 'mypy')
   lint.linters.mypy.cmd = python_mypy_cmd
@@ -37,9 +39,12 @@ if vim.fn.executable(python_mypy_cmd) == 1 then
     "--namespace-packages"
   )
 
+  -- Set additional entries in MYPYPATH.
   local mypy_path = extend_mypy_path({
-    -- Additional entries in MYPYPATH.
-    -- vim.fn.expand("~/additional/path"),
+    ".venv/lib/python3.7/site-packages",
+    ".venv/lib/python3.8/site-packages",
+    ".venv/lib/python3.9/site-packages",
+    ".venv/lib/python3.10/site-packages",
   })
 
   lint.linters.mypy.env = {
@@ -52,9 +57,11 @@ lint.linters_by_ft = {
   python = python_linters
 }
 
+local function try_lint()
+  lint.try_lint()
+end
+
 -- Trigger linting on file save.
 vim.api.nvim_create_autocmd({"BufWinEnter", "BufWritePost"}, {
-  callback = function()
-    lint.try_lint()
-  end,
+  callback = try_lint,
 })
