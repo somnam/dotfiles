d8ad6b0f138d5abdb398bf6b7dc6ba2b1aa557b8
Tomek Miodek <tomek.miodek@gmail.com>
2022-11-05 14:02:35 +0100

    rc updates

--- home/tomek/.config/bashrc.d/pyenv.bash
+++ home/tomek/.config/bashrc.d/pyenv.bash
@@ -7,6 +7,6 @@ if [[ -x ${PYENV_ROOT}/bin/pyenv ]]; then
     command -v pyenv >/dev/null || PATH="${PYENV_ROOT}/bin:${PATH}"
 
     # Initialize pyenv
-    eval "$(pyenv init -)"
-    eval "$(pyenv virtualenv-init -)"
+    eval -- "$(pyenv init -)"
+    eval -- "$(pyenv init --no-rehash -)"
 fi
