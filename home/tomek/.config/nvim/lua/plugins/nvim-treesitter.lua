f4ae7ee716366d8b01c13cabdaf93084d8ac4165
Tomek Miodek <tomek.miodek@gmail.com>
2022-11-05 14:05:13 +0100

    Nvim updates

--- home/tomek/.config/nvim/lua/plugins/nvim-treesitter.lua
+++ home/tomek/.config/nvim/lua/plugins/nvim-treesitter.lua
@@ -2,15 +2,6 @@ local available, _ = pcall(require, "nvim-treesitter")
 if not available then return end
 
 require('nvim-treesitter.configs').setup({
-  ensure_installed = {
-    "bash",
-    "lua",
-    "make",
-    "markdown",
-    "python",
-    "toml",
-  },
-  sync_install = true,
   highlight = {
     enable = true,
     disable = { "json", "yaml" },
