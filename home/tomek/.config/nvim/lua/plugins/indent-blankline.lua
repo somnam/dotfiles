280ded75b7ce387d8bf69023d3e2f42cc307772e
Tomek Miodek <tomek.miodek@gmail.com>
2022-11-07 18:04:59 +0100

    Nvim updates

--- home/tomek/.config/nvim/lua/plugins/indent-blankline.lua
+++ home/tomek/.config/nvim/lua/plugins/indent-blankline.lua
@@ -2,6 +2,6 @@ local available, indent_blankline = pcall(require, "indent_blankline")
 if not available then return end
 
 indent_blankline.setup({
-  use_treesitter = true,
   show_current_context = true,
+  show_trailing_blankline_indent = false,
 })
