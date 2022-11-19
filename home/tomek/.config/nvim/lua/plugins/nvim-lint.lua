6079910eed53514233b749ca66c7eff935f96a40
Tomek Miodek <tomek.miodek@gmail.com>
2022-11-19 17:45:15 +0100

    Nvim updates

--- home/tomek/.config/nvim/lua/plugins/nvim-lint.lua
+++ home/tomek/.config/nvim/lua/plugins/nvim-lint.lua
@@ -8,7 +8,7 @@ local vim_data_path = vim.fn.stdpath("data")
 local flake8_cmds = {"flake8", vim_data_path .. "/python/bin/flake8"}
 for _, flake8_cmd in ipairs(flake8_cmds) do
   if vim.fn.executable(flake8_cmd) == 1 then
-    table.insert(python_linters, 'flake8')
+    table.insert(python_linters, "flake8")
     lint.linters.flake8.cmd = flake8_cmd
     break
   end
@@ -17,7 +17,7 @@ end
 local mypy_cmds = {"mypy", vim_data_path .. "/python/bin/mypy"}
 for _, mypy_cmd in ipairs(mypy_cmds) do
   if vim.fn.executable(mypy_cmd) == 1 then
-    table.insert(python_linters, 'mypy')
+    table.insert(python_linters, "mypy")
     lint.linters.mypy.cmd = mypy_cmd
     -- Allow namespace packages.
     table.insert(lint.linters.mypy.args, "--namespace-packages")
