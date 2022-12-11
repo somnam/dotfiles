08bf1e91c3c01da2203d6cc074c10f28c2a16d9e
Tomek Miodek <tomek.miodek@gmail.com>
2022-12-11 14:17:38 +0100

    Nvim updates

--- home/tomek/.config/nvim/lua/plugins/alpha-nvim.lua
+++ home/tomek/.config/nvim/lua/plugins/alpha-nvim.lua
@@ -27,7 +27,7 @@ dashboard.section.header.val = {
 
 dashboard.section.buttons.val = {
     dashboard.button("<Leader> fp", "> Previous files" , ":FzfLua oldfiles<Enter>"),
-    dashboard.button("<Leader> ff", "> Find files", ":FzfLua oldfiles<Enter>"),
+    dashboard.button("<Leader> ff", "> Find files", ":FzfLua files<Enter>"),
     dashboard.button("<Leader> fw", "> Find in files", ":FzfLua live_grep<Enter>"),
     dashboard.button("<Leader> fg", "> Find changed files", ":FzfLua git_status<Enter>"),
     dashboard.button("<Leader> n ", "> New file" , ":enew<Enter>"),
