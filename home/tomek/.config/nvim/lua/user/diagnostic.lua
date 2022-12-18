5cf683f571c069a41419c975ac4d0bb9f4f7f2fa
Tomek Miodek <tomek.miodek@gmail.com>
2022-12-18 15:06:11 +0100

    Nvim updates

--- home/tomek/.config/nvim/lua/user/diagnostic.lua
+++ home/tomek/.config/nvim/lua/user/diagnostic.lua
@@ -1,8 +1,8 @@
 local opts = {noremap = true, silent = true}
-vim.api.nvim_set_keymap("n", "<Space>d[", ":lua vim.diagnostic.goto_prev()<Enter>", opts)
-vim.api.nvim_set_keymap("n", "<Space>d]", ":lua vim.diagnostic.goto_next()<Enter>", opts)
-vim.api.nvim_set_keymap("n", "<Space>dm", ":lua vim.diagnostic.setloclist()<Enter>", opts)
-vim.api.nvim_set_keymap("n", "<Space>dd", ":lua vim.diagnostic.open_float()<Enter>", opts)
+vim.api.nvim_set_keymap("n", "<Space>[m", ":lua vim.diagnostic.goto_prev()<Enter>", opts)
+vim.api.nvim_set_keymap("n", "<Space>]m", ":lua vim.diagnostic.goto_next()<Enter>", opts)
+vim.api.nvim_set_keymap("n", "<Space>m", ":lua vim.diagnostic.open_float()<Enter>", opts)
+vim.api.nvim_set_keymap("n", "<Space>M", ":lua vim.diagnostic.setloclist()<Enter>", opts)
 
 local diagnostic_opts = {
   virtual_text = {
