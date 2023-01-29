9c899eb94ec8d2158ac04a00c3c7f2ac75ace989
Tomek Miodek <tomek.miodek@gmail.com>
2023-01-29 16:05:28 +0100

    Nvim updates

--- home/tomek/.config/nvim/lua/plugin/packer.lua
+++ home/tomek/.config/nvim/lua/plugin/packer.lua
@@ -1,14 +1,3 @@
 local P = {"wbthomason/packer.nvim"}
 
-P.config = function()
-  -- autocmd
-  vim.api.nvim_create_autocmd("BufWritePost", {
-    pattern = "*/nvim/lua/plugin/init.lua",
-    group = vim.api.nvim_create_augroup("packer_auto_source", { clear = true }),
-    callback = function(ctx)
-      vim.cmd("source " .. ctx.file)
-    end
-  })
-end
-
 return P
