5231dae7d75ece3041283ec64cf9a86ef8dd9db7
Tomek Miodek <tomek.miodek@gmail.com>
2023-02-08 20:17:18 +0100

    Nvim updates

--- home/tomek/.config/nvim/lua/rc/options.lua
+++ home/tomek/.config/nvim/lua/rc/options.lua
@@ -19,9 +19,17 @@ vim.opt.wrap = true             -- Line wrapping
 vim.opt.linebreak = true        -- Wrap long lines at a linebreak character
 vim.opt.wrapscan =  false       -- Search wrapping
 vim.opt.foldmethod = "marker"   -- Set folding method
-vim.opt.cursorline = true       -- Highlight current cursor line
+vim.opt.foldmarker = {          -- Set fold marker
+  "# region",
+  "# endregion"
+}
+vim.opt.foldenable = false      -- No autofold
+vim.opt.cursorline = true       -- Highlight current line
+vim.opt.cursorlineopt = "line"
 vim.opt.cursorcolumn = false    -- Don't highlight current cursor column
+vim.opt.signcolumn = "yes"      -- Show sign column
 vim.opt.number = false          -- Don't display line numbers
+vim.opt.hidden = true           -- Hide buffer instead of closing it
 vim.opt.mouse = "vi"            -- Mouse modes
 
 vim.opt.smartindent = true      -- Try to be smart with indenting
@@ -31,23 +39,23 @@ vim.opt.smarttab = true         -- Smart tab
 vim.opt.shiftwidth = 4          -- Set default tabs behavior
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.signcolumn = "yes"      -- Show sign column
-vim.opt.hidden = true           -- Hide buffer instead of closing it
 
-vim.opt.lazyredraw = true       -- Don't redraw the screen when not needed
 vim.opt.ttyfast = true          -- Faster scrolling
-vim.opt.ttimeout = true         -- Time waited for key press(es) to complete, faster key response
 vim.opt.timeout = true          -- Setting for mappings
+vim.opt.ttimeout = true         -- Time waited for key press(es) to complete, faster key response
 vim.opt.timeoutlen = 1000       -- Set to default value
-vim.opt.ttimeoutlen = 10        -- Set to unnoticeable small value
-vim.opt.updatetime = 300        -- Longer update time leads to noticeable delays
+vim.opt.ttimeoutlen = 25        -- Set to unnoticeable small value
+vim.opt.updatetime = 1000       -- Longer update time leads to noticeable delays
 vim.opt.synmaxcol = 512         -- Be forgiving with long lines
 
 vim.opt.clipboard = "unnamedplus" -- Use the "global" buffer for copy and paste
 vim.opt.undofile = true         -- Enable persistent undo
 vim.opt.modeline = false        -- Don't run arbitrary modeline code
 vim.opt.inccommand = "nosplit"  -- Shows the effects of a command incrementally.
+
 vim.g.netrw_banner = 0          -- Hide netrw banner
+vim.g.loaded_netrw = 1          -- Disable netrw
+vim.g.loaded_netrwPlugin = 1
 
 vim.opt.formatoptions:remove("c")   -- Disable automatic comment insertion
 vim.opt.formatoptions:remove("r")
