04836bef61477439b6a167fb435d4948cea71d09
Tomek Miodek <tomek.miodek@gmail.com>
2023-04-08 18:32:53 +0200

    Bashrc changes

--- home/tomek/.config/bashrc.d/aliases/brew.sh
+++ home/tomek/.config/bashrc.d/aliases/brew.sh
@@ -1,3 +1,15 @@
-alias brew-out='brew update && brew outdated -g'
-alias brew-up="brew update && brew upgrade -g && brew autoremove && brew cleanup --prune=all"
-alias brew-clean="brew autoremove && brew cleanup --prune=all"
+brew-out() {
+    brew update && brew outdated -g
+}
+
+brew-up() {
+    brew update && brew upgrade -g && brew-clean
+}
+
+brew-prune() {
+    brew uninstall --zap $@ && brew-clean
+}
+
+brew-clean() {
+    brew autoremove && brew cleanup --prune=all
+}
