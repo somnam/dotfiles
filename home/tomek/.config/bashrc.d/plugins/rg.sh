490384487da2d50d2974eb8cb6545fc11bfd77fd
Tomek Miodek <tomek.miodek@gmail.com>
2023-04-08 18:32:53 +0200

    Bashrc changes

--- home/tomek/.config/bashrc.d/plugins/rg.sh
+++ home/tomek/.config/bashrc.d/plugins/rg.sh
@@ -1,9 +1,9 @@
 if command -v rg > /dev/null 2>&1; then
-    rg-py() {
+    rgpy() {
         rg --type py --smart-case $@
     }
 
-    rg-f() {
+    rgf() {
         if [ -z "$2" ]; then
             rg --files . | rg "$1"
         else
