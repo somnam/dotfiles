5d640978cf16486b0609743db02d8c8c700bbe36
Tomek Miodek <tomek.miodek@gmail.com>
2023-06-02 21:46:12 +0200

    Switch packer to lazy.nvim

--- home/tomek/.config/nvim/lua/plugin/init.lua
+++ home/tomek/.config/nvim/lua/plugin/init.lua
@@ -1,46 +1,57 @@
-local available, packer = pcall(require, "packer")
-if not available then return end
+local git_available = vim.fn.executable("git") == 1
+if not git_available then return end
 
 local H = {}
 
-H.path = vim.fn.stdpath('config') .. "/lua/plugin/"
+H.lazy_path = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
-H.source_plugins = function(_)
-  vim.cmd("source " .. H.path .. "init.lua")
+H.lazy_url = "https://github.com/folke/lazy.nvim.git"
+
+H.bootstrap = function()
+  if not vim.loop.fs_stat(H.lazy_path) then
+    vim.fn.system({
+      "git",
+      "clone",
+      "--filter=blob:none",
+      H.lazy_url,
+      "--branch=stable", -- latest stable release
+      H.lazy_path,
+    })
+  end
+
+  vim.opt.rtp:prepend(H.lazy_path)
 end
 
-H.plugin_file = function(filename)
-  return H.path .. filename .. ".lua"
+H.icons = {
+  cmd = "⌘",
+  config = "🛠",
+  event = "📅",
+  ft = "📂",
+  init = "⚙",
+  import = "✅",
+  keys = "🗝",
+  plugin = "🔌",
+  runtime = "💻",
+  source = "📄",
+  start = "🚀",
+  task = "📌",
+  lazy = "💤 ",
+}
+
+H.setup = function()
+  -- keymap
+  local opts = {noremap = true, silent = true}
+  vim.api.nvim_set_keymap("n", "<Space>l", ":Lazy<Enter>", opts)
+
+  require("lazy").setup(
+    "plugin.spec",
+    {
+      ui = {
+        icons = H.icons,
+      }
+    }
+  )
 end
 
-H.plugin = function(filename)
-  local plugin_available, plugin = pcall(dofile, H.plugin_file(filename))
-  return plugin_available and plugin or nil
-end
-
--- autocmd
-vim.api.nvim_create_autocmd("BufWritePost", {
-  pattern = "*/nvim/lua/plugin/*.lua",
-  group = vim.api.nvim_create_augroup("packer_auto_source", { clear = true }),
-  callback = H.source_plugins,
-})
-
-packer.startup(function(use)
-  use {"wbthomason/packer.nvim"}
-  use (H.plugin("mini"))
-  use (H.plugin("alpha-nvim"))
-  use (H.plugin("guess-indent"))
-  use (H.plugin("indent-blankline"))
-  use (H.plugin("nvim-lspconfig"))
-  use (H.plugin("gitsigns"))
-  use (H.plugin("nvim-treesitter"))
-  use (H.plugin("nvim-lint"))
-  use (H.plugin("formatter"))
-  use (H.plugin("lualine"))
-  use (H.plugin("fzf-lua"))
-  use (H.plugin("nvim-tree"))
-  use (H.plugin("nvim-cmp"))
-  use (H.plugin("vscode"))
-  use (H.plugin("dracula"))
-  use (H.plugin("tokyonight"))
-end)
+H.bootstrap()
+H.setup()
