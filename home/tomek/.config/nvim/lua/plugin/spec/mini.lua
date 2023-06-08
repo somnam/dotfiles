793e10f7ce56a31a9925f588756bc077f0e1164c
Tomek Miodek <tomek.miodek@gmail.com>
2023-06-08 14:41:34 +0200

    Switch packer to lazy.nvim

--- home/tomek/.config/nvim/lua/plugin/spec/mini.lua
+++ home/tomek/.config/nvim/lua/plugin/spec/mini.lua
@@ -1,6 +1,23 @@
 local P = {"echasnovski/mini.nvim"}
 
+P.event = "VeryLazy"
+
 P.config = function()
+  local buffer = require("util.buffer")
+
+  local H = {}
+
+  H.exclude_filetype = buffer.exclude.filetype
+
+  H.max_size = buffer.max_size
+
+  H.maybe_disable_mini_cursorword = function(ctx)
+    return (
+      vim.tbl_contains(H.exclude_filetype, ctx.match)
+      or buffer.above_max_size(ctx.buf, H.max_size)
+    )
+  end
+
   local mini_bufremove = require("mini.bufremove")
   vim.api.nvim_cmd({
     cmd = "command",
@@ -33,6 +50,19 @@ P.config = function()
 
   local mini_trailspace = require("mini.trailspace")
   mini_trailspace.setup()
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "*",
+    group = vim.api.nvim_create_augroup("mini_cursorword_file_disable", { clear = true }),
+    callback = function(ctx)
+      if H.maybe_disable_mini_cursorword(ctx) then
+        vim.b.minicursorword_disable = true
+      end
+    end
+  })
+
+  local mini_cursorword = require("mini.cursorword")
+  mini_cursorword.setup({delay = 150})
 end
 
 return P
