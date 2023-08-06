7c97485cc3405b20ac23596832adc26184424e23
Tomek Miodek <tomek.miodek@gmail.com>
2023-08-06 11:54:41 +0200

    Add nvm plugin

--- home/tomek/.config/bashrc.d/plugins/nvm.sh
+++ home/tomek/.config/bashrc.d/plugins/nvm.sh
@@ -1,4 +1,5 @@
 if [ -d ${HOME}/.nvm ]; then
     export NVM_DIR=${HOME}/.nvm
     [[ -s "$NVM_DIR/nvm.sh" ]] && source $NVM_DIR/nvm.sh
+    [[ -r $NVM_DIR/bash_completion ]] && source $NVM_DIR/bash_completion
 fi
