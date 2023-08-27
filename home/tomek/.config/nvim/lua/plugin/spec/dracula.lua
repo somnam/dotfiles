23090731e0c5f5b93eb85c0adda9ede168035328
Tomek Miodek <tomek.miodek@gmail.com>
2023-08-27 18:33:36 +0200

    Update dracula color

--- home/tomek/.config/nvim/lua/plugin/spec/dracula.lua
+++ home/tomek/.config/nvim/lua/plugin/spec/dracula.lua
@@ -5,10 +5,10 @@ P.event = "ColorSchemePre dracula"
 P.config = function()
   -- autocmd
   vim.api.nvim_create_autocmd("ColorScheme", {
-    pattern = "dracula",
+    pattern = "dracula*",
     group = vim.api.nvim_create_augroup("set_dracula_theme", { clear = true }),
     callback = function()
-      vim.api.nvim_set_hl(0, "CursorLine", {bg="#343746"})
+      vim.api.nvim_set_hl(0, "CursorLine", {bg="#282A36"})
     end
   })
 
