3e2e02601417c71ba8d35b73890435838d9307a9
Tomek Miodek <tomek.miodek@gmail.com>
2023-10-10 06:58:54 +0200

    Bash and alacritty updates

--- home/tomek/.config/bashrc.d/aliases/brew.sh
+++ home/tomek/.config/bashrc.d/aliases/brew.sh
@@ -1,9 +1,9 @@
 brew-out() {
-    brew update && brew outdated -g
+    brew update && brew outdated --greedy
 }
 
 brew-up() {
-    brew update && brew upgrade -g && brew-clean
+    brew update && brew upgrade --greedy --no-quarantine && brew-clean
 }
 
 brew-prune() {
