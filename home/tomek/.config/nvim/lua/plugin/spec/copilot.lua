7e540866b4ac566ebdfed1e04be5b284ceca19c1
Tomek Miodek <tomek.miodek@gmail.com>
2023-12-11 21:47:11 +0100

    Trying copilot

--- home/tomek/.config/nvim/lua/plugin/spec/copilot.lua
+++ home/tomek/.config/nvim/lua/plugin/spec/copilot.lua
@@ -1,10 +1,12 @@
 return {
   "zbirenbaum/copilot.lua",
   cmd = "Copilot",
-  event = "InsertEnter",
-  enabled = false,
+  event = {"BufReadPost", "BufNewFile"},
+  enabled = true,
   config = function()
     local copilot_suggestion = require("copilot.suggestion")
+    local copilot_client = require("copilot.client")
+    local copilot_command = require("copilot.command")
 
     local H = {}
 
@@ -20,12 +22,49 @@ return {
       end
     end
 
+    H.enable_copilot = function()
+      if copilot_client.is_disabled() then
+        copilot_command.enable()
+      end
+      copilot_command.attach({force = true})
+    end
+
+    H.toggle_copilot = function()
+      if not copilot_client.is_disabled() then
+        copilot_command.toggle({force = true})
+      end
+    end
+
+    H.disable_copilot = function()
+      if not copilot_client.is_disabled() then
+        copilot_command.detach()
+        copilot_command.disable()
+      end
+    end
+
+    -- keymap
+    local opts = {noremap = true, silent = true}
     for _, mode in ipairs({"i", "s"}) do
       vim.api.nvim_set_keymap(
         mode, "<Tab>", "",
-        {noremap = true, silent = true, callback = H.accept_suggestion}
+        vim.tbl_extend("keep", {callback = H.accept_suggestion}, opts)
       )
     end
+
+    vim.api.nvim_set_keymap("n", "<Space>ce", "", vim.tbl_extend("keep", opts, {
+      callback = H.enable_copilot,
+      desc = "Enable Copilot for current buffer.",
+    }))
+
+    vim.api.nvim_set_keymap("n", "<Space>ct", "", vim.tbl_extend("keep", opts, {
+      callback = H.toggle_copilot,
+      desc = "Toggle Copilot for current buffer.",
+    }))
+
+    vim.api.nvim_set_keymap("n", "<Space>cd", "", vim.tbl_extend("keep", opts, {
+      callback = H.disable_copilot,
+      desc = "Disable Copilot for all buffers.",
+    }))
 
     -- setup
     require("copilot").setup({
@@ -36,6 +75,9 @@ return {
       },
       panel = {
         enabled = false,
+      },
+      filetypes = {
+        ["*"] = false,
       }
     })
   end
