85e475b5f91b3fd65406ec65d6f215b94485945e
Tomek Miodek <tomek.miodek@gmail.com>
2023-12-30 08:58:28 +0100

    Copilot update

--- home/tomek/.config/nvim/lua/plugin/copilot.lua
+++ home/tomek/.config/nvim/lua/plugin/copilot.lua
@@ -50,26 +50,45 @@ H.disable_copilot = function()
 end
 
 return {
-  "zbirenbaum/copilot.lua",
-  cmd = "Copilot",
-  cond = function()
-    return require("util.command").executable("node")
-  end,
-  keys = {
-    {"<Space>ce", H.enable_copilot, desc = "Enable Copilot for current buffer."},
-    {"<Space>cd", H.disable_copilot, desc = "Disable Copilot for all buffers."},
-  },
-  opts = {
-    suggestion = {
-      auto_trigger = true,
-      keymap = {accept = false},
-      debounce = 150,
+  {
+    "zbirenbaum/copilot.lua",
+    commit = "38a41d0",
+    cmd = "Copilot",
+    cond = function()
+      return require("util.command").executable("node")
+    end,
+    dependencies = {"AndreM222/copilot-lualine"},
+    keys = {
+      {"<Space>ce", H.enable_copilot, desc = "Enable Copilot for current buffer."},
+      {"<Space>cd", H.disable_copilot, desc = "Disable Copilot for all buffers."},
     },
-    panel = {
-      enabled = false,
-    },
-    filetypes = {
-      ["*"] = false,
+    opts = {
+      suggestion = {
+        auto_trigger = true,
+        keymap = {accept = false},
+        debounce = 150,
+      },
+      panel = {
+        enabled = false,
+      },
+      filetypes = {
+        ["*"] = false,
+      }
     }
-  }
+  },
+  {
+    "AndreM222/copilot-lualine",
+    lazy = true,
+    dependencies = {"nvim-lualine/lualine.nvim"},
+    config = function ()
+      local lualine_config = require("lualine_require").lazy_require({
+        config_module = "lualine.config"
+      })
+      local current_config = lualine_config.config_module.get_config()
+
+      table.insert(current_config.sections.lualine_c, "copilot")
+
+      require("lualine").setup(current_config)
+    end
+  },
 }
