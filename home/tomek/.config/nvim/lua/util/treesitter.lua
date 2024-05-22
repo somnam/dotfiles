13df110c07a7009650753bd81335d9e55aa92fdc
Tomek Miodek <tomek.miodek@gmail.com>
2024-05-22 23:17:34 +0200

    Nvim 0.10 updates

--- home/tomek/.config/nvim/lua/util/treesitter.lua
+++ home/tomek/.config/nvim/lua/util/treesitter.lua
@@ -1,9 +1,10 @@
 local buffer = require("util.buffer")
+local config = require("core.config")
 
 local H = {}
 
 H.max_size = 1024 * 128
 
-H.exclude_filetype = vim.list_extend({ "csv" }, buffer.exclude.filetype)
+H.exclude_filetype = vim.list_extend(config.treesitter.exclude or {}, buffer.exclude.filetype)
 
 return H
