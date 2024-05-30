7ce06538b230edc56faff1700d60227b7aa6fb05
Tomek Miodek <tomek.miodek@gmail.com>
2024-05-30 21:20:44 +0200

    Bash updates

--- home/tomek/.config/bashrc.d/plugins/pyenv.sh
+++ home/tomek/.config/bashrc.d/plugins/pyenv.sh
@@ -1,10 +1,5 @@
 if [ -d ${HOME}/.pyenv ]; then
-    # Lazy load pyenv
-    function pyenv() {
-        unset -f pyenv
-        export PYENV_ROOT=$HOME/.pyenv
-        export PATH="${PYENV_ROOT}/bin:${PATH}"
-        eval "$(command pyenv init -)"
-        pyenv "$@"
-    }
+    export PYENV_ROOT=$HOME/.pyenv
+    export PATH="${PYENV_ROOT}/bin:${PATH}"
+    eval -- "$(command pyenv init --no-rehash -)"
 fi
