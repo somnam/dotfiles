a48f8b6818381a1078f4ce93463df716909539ad
Tomek Miodek <tomek.miodek@gmail.com>
2024-06-12 18:17:56 +0200

    Nvim 0.10 updates

--- home/tomek/.config/nvim/lua/plugin/colorschemes.lua
+++ home/tomek/.config/nvim/lua/plugin/colorschemes.lua
@@ -1,21 +1,5 @@
 return {
   {
-    "Mofiqul/dracula.nvim",
-    event = "ColorSchemePre dracula",
-    init = function()
-      vim.api.nvim_create_autocmd("ColorScheme", {
-        pattern = "dracula*",
-        group = vim.api.nvim_create_augroup("set_dracula_theme", { clear = true }),
-        callback = function()
-          vim.api.nvim_set_hl(0, "CursorLine", { bg = "#343746" })
-        end,
-      })
-    end,
-    opts = {
-      transparent_bg = true,
-    },
-  },
-  {
     "Mofiqul/vscode.nvim",
     event = "ColorSchemePre vscode",
     init = function()
@@ -35,5 +19,9 @@ return {
       keywordStyle = { italic = false },
       statementStyle = { bold = false },
     },
+  },
+  {
+    "EdenEast/nightfox.nvim",
+    event = "ColorSchemePre *fox",
   },
 }
