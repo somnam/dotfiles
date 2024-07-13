47ec0f368f0b9f34e7a4916626ddf076ff6177a3
Tomek Miodek <tomek.miodek@gmail.com>
2024-07-13 17:01:21 +0200

    Bashrc changes

--- home/tomek/.config/bashrc.d/plugins/nvm.sh
+++
@@ -1,5 +0,0 @@
-if [ -d ${HOME}/.nvm ]; then
-    export NVM_DIR=${HOME}/.nvm
-    [[ -s "$NVM_DIR/nvm.sh" ]] && source $NVM_DIR/nvm.sh
-    [[ -r $NVM_DIR/bash_completion ]] && source $NVM_DIR/bash_completion
-fi
