e3521f197bac2cee2af7ac4c34d682e76ee846a2
Tomek Miodek <tomek.miodek@gmail.com>
2024-08-13 21:02:50 +0200

    Nvim updates

--- home/tomek/.config/nvim/lua/plugin/nvim-lspconfig.lua
+++ home/tomek/.config/nvim/lua/plugin/nvim-lspconfig.lua
@@ -2,85 +2,72 @@ local command = require("util.command")
 local misc = require("util.misc")
 local python = require("util.python")
 
+local servers = {
+  jedi_language_server = {
+    init_options = {
+      completion = {
+        disableSnippets = true,
+      },
+    },
+    on_attach = function(client, bufnr)
+      -- Customize trigger characters.
+      client.server_capabilities.completionProvider.triggerCharacters = { "." }
+    end,
+  },
+  pyright = {
+    settings = {
+      pyright = {
+        disableLanguageServices = false,
+        disableOrganizeImports = true,
+      },
+      python = {
+        analysis = {
+          autoImportCompletions = true,
+          autoSearchPaths = true,
+          diagnosticMode = "openFilesOnly",
+          typeCheckingMode = "off",
+          useLibraryCodeForTypes = true,
+        },
+        pythonPath = python.virtual_env_cmd("python"),
+      },
+    },
+  },
+  quick_lint_js = {
+    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
+  },
+  rust_analyzer = {
+    settings = {
+      ["rust-analyzer"] = {
+        check = {
+          command = "clippy",
+        },
+        completion = {
+          postfix = { enable = false },
+        },
+      },
+    },
+  },
+  lua_ls = {
+    on_attach = function(client, bufnr)
+      client.server_capabilities.completionProvider.triggerCharacters =
+        { ".", ":", "(", "[", ",", "#", "*", "@", "|", "=", "{", "+", "?" }
+    end,
+  },
+}
+
 return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
-  dependencies = { "williamboman/mason.nvim" },
   opts = function(_, opts)
-    return misc.deep_extend({
+    return vim.tbl_deep_extend("force", {
       capabilities = vim.lsp.protocol.make_client_capabilities(),
-      servers = {
-        jedi_language_server = {
-          init_options = {
-            completion = {
-              disableSnippets = true,
-            },
-          },
-          on_attach = function(client, bufnr)
-            -- Customize trigger characters.
-            client.server_capabilities.completionProvider.triggerCharacters = { "." }
-          end,
-        },
-        pyright = {
-          settings = {
-            pyright = {
-              disableLanguageServices = false,
-              disableOrganizeImports = true,
-            },
-            python = {
-              analysis = {
-                autoImportCompletions = true,
-                autoSearchPaths = true,
-                diagnosticMode = "openFilesOnly",
-                typeCheckingMode = "off",
-                useLibraryCodeForTypes = true,
-              },
-              pythonPath = python.virtual_env_cmd("python"),
-            },
-          },
-        },
-        quick_lint_js = {},
-        rust_analyzer = {
-          settings = {
-            ["rust-analyzer"] = {
-              check = {
-                command = "clippy",
-              },
-              completion = {
-                postfix = { enable = false },
-              },
-            },
-          },
-        },
-        lua_ls = {
-          on_attach = function(client, bufnr)
-            client.server_capabilities.completionProvider.triggerCharacters =
-              { ".", ":", "(", "[", ",", "#", "*", "@", "|", "=", "{", "+", "?" }
-          end,
-        },
-      },
+      servers = servers,
     }, opts)
   end,
   config = function(_, opts)
     local lspconfig = require("lspconfig")
-    local lspconfig_configs = require("lspconfig.configs")
 
-    lspconfig_configs.quick_lint_js = {
-      default_config = {
-        cmd = { "quick-lint-js", "--lsp" },
-        filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact" },
-        root_dir = function(filename)
-          local root = lspconfig.util.path.dirname(filename)
-          lspconfig.util.path.traverse_parents(filename, function(dir, _)
-            root = dir
-          end)
-          return root
-        end,
-      },
-      docs = { description = "quick-lint-js" },
-    }
-
-    for server, server_config in pairs(opts.servers) do
+    for server, server_config in pairs(servers) do
       local cmd = lspconfig[server].document_config.default_config.cmd
       if type(cmd) == "table" and command.executable(cmd[1]) then
         lspconfig[server].setup(
