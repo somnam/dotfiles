64ee27e6a37c63aa2b783e27f333f30171ea7520
Tomek Miodek <tomek.miodek@gmail.com>
2024-09-01 22:37:12 +0200

    Nvim updates

--- home/tomek/.config/nvim/lua/plugin/nvim-cmp.lua
+++ home/tomek/.config/nvim/lua/plugin/nvim-cmp.lua
@@ -53,16 +53,19 @@ return {
       return abbr
     end
 
-    H.buffer_option = {
-      get_bufnrs = H.get_bufnrs,
-      indexing_interval = 500,
-    }
-
     return {
       sources = {
         { name = "nvim_lsp", group_index = 1 },
-        { name = "buffer", group_index = 2, keyword_length = 2, option = H.buffer_option },
-        { name = "path", group_index = 3, keyword_length = 2 },
+        {
+          name = "buffer",
+          group_index = 2,
+          keyword_length = 3,
+          option = {
+            get_bufnrs = H.get_bufnrs,
+            indexing_interval = 500,
+          },
+        },
+        { name = "path", group_index = 3 },
       },
       enabled = H.current_buffer_enabled,
       confirm_opts = {
