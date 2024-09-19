fae73bfbc1cb661d9b250a519df3cbd37fe6e1b2
Tomek Miodek <tomek.miodek@gmail.com>
2024-09-19 08:25:47 +0200

    Nvim updates

--- home/tomek/.config/nvim/lua/core/lsp.lua
+++ home/tomek/.config/nvim/lua/core/lsp.lua
@@ -1,21 +1,27 @@
 local opts = { noremap = true, silent = true }
 vim.api.nvim_set_keymap(
   "n",
-  "<Space>r",
+  "grn",
   ":lua vim.lsp.buf.rename()<Enter>",
   vim.tbl_extend("keep", { desc = "Rename all symbol references" }, opts)
 )
 vim.api.nvim_set_keymap(
   "n",
-  "<Space>o",
+  "grs",
   ":lua vim.lsp.buf.document_symbol()<Enter>",
   vim.tbl_extend("keep", { desc = "List all current buffer symbols" }, opts)
 )
 vim.api.nvim_set_keymap(
   "n",
-  "<Space>a",
+  "grr",
   ":lua vim.lsp.buf.references()<Enter>",
   vim.tbl_extend("keep", { desc = "List all symbol references" }, opts)
+)
+vim.api.nvim_set_keymap(
+  "n",
+  "gra",
+  ":lua vim.lsp.buf.code_action()<Enter>",
+  vim.tbl_extend("keep", { desc = "Select code action at cursor position" }, opts)
 )
 vim.api.nvim_set_keymap(
   "i",
@@ -24,13 +30,52 @@ vim.api.nvim_set_keymap(
   vim.tbl_extend("keep", { desc = "Display signature of the symbol" }, opts)
 )
 
-local handlers = {
-  ["textDocument/hover"] = vim.lsp.handlers.hover,
-  ["textDocument/signatureHelp"] = vim.lsp.handlers.signature_help,
-}
-local handlers_style = { focusable = true, style = "minimal", border = "rounded" }
-for name, handler in pairs(handlers) do
-  vim.lsp.handlers[name] = vim.lsp.with(handler, handlers_style)
+local H = {}
+
+H.set_lsp_handlers_style = function()
+  local handlers = {
+    ["textDocument/hover"] = vim.lsp.handlers.hover,
+    ["textDocument/signatureHelp"] = vim.lsp.handlers.signature_help,
+  }
+  local handlers_style = { focusable = true, style = "minimal", border = "rounded" }
+  for name, handler in pairs(handlers) do
+    vim.lsp.handlers[name] = vim.lsp.with(handler, handlers_style)
+  end
 end
 
-vim.lsp.set_log_level("ERROR")
+H.set_lsp_options = function()
+  vim.lsp.set_log_level("ERROR")
+end
+
+H.setup_lsp_highlight_symbol = function()
+  local function setup_lsp_highlight_symbol(event)
+    local client_id = vim.tbl_get(event, "data", "client_id")
+    local client = client_id and vim.lsp.get_client_by_id(client_id)
+    if not (client and client.supports_method("textDocument/documentHighlight")) then
+      return
+    end
+
+    local group = vim.api.nvim_create_augroup("lsp_highlight_symbol", { clear = false })
+    vim.api.nvim_clear_autocmds({ buffer = event.buf, group = group })
+    vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+      group = group,
+      buffer = event.buf,
+      callback = vim.lsp.buf.document_highlight,
+    })
+    vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+      group = group,
+      buffer = event.buf,
+      callback = vim.lsp.buf.clear_references,
+    })
+  end
+
+  vim.api.nvim_create_autocmd("LspAttach", {
+    pattern = "*",
+    group = vim.api.nvim_create_augroup("lsp_setup_highlight_symbol", { clear = true }),
+    callback = setup_lsp_highlight_symbol,
+  })
+end
+
+H.set_lsp_options()
+H.set_lsp_handlers_style()
+H.setup_lsp_highlight_symbol()
