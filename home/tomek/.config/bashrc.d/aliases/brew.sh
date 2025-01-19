ca12196fe3b4d709d15f37d3264c08660eef495c
Tomek Miodek <tomek.miodek@gmail.com>
2025-01-19 13:23:22 +0100

    Staged changes

--- home/tomek/.config/bashrc.d/aliases/brew.sh
+++ home/tomek/.config/bashrc.d/aliases/brew.sh
@@ -1,10 +1,10 @@
 if command -v brew >/dev/null; then
     brew-out() {
-        brew update && brew outdated
+        brew update && brew outdated $@
     }
 
     brew-up() {
-        brew update && brew upgrade && brew autoremove
+        brew update && brew upgrade $@ && brew autoremove
     }
 
     brew-prune() {
