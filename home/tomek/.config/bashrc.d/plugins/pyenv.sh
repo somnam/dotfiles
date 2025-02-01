6ce541f534467a3544a8cbc8c7598213c659f877
Tomek Miodek <tomek.miodek@gmail.com>
2025-02-01 08:47:25 +0100

    Staged changes

--- home/tomek/.config/bashrc.d/plugins/pyenv.sh
+++ home/tomek/.config/bashrc.d/plugins/pyenv.sh
@@ -2,8 +2,8 @@ if [ -d ${HOME}/.pyenv ]; then
     export PYENV_ROOT=$HOME/.pyenv
     export PATH="${PYENV_ROOT}/bin:${PATH}"
     if [[ ":$PATH:" != *":$HOME/.pyenv/bin:"* ]]; then
-        eval -- "$(command pyenv init --no-rehash -)"
+        eval -- "$(command pyenv init -)"
     else
-        eval -- "$(command pyenv init --no-push-path --no-rehash -)"
+        eval -- "$(command pyenv init --no-push-path -)"
     fi
 fi
