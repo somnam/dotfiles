6e0bbb3c641fb6fb8058762960251da3167a15d6
Tomek Miodek <tomek.miodek@gmail.com>
2025-05-25 13:45:36 +0200

    Staged changes

--- home/tomek/.config/nvim/lua/core/config.lua
+++ home/tomek/.config/nvim/lua/core/config.lua
@@ -7,7 +7,11 @@ M.config = require("util.file").read_json_file(vim.fn.stdpath("config") .. "/.co
 ---@param default any?
 ---@return any
 M.get = function(location, default)
-  return vim.tbl_get(M.config, unpack(vim.split(location, "%."))) or default
+  local option = vim.tbl_get(M.config, unpack(vim.split(location, "%.")))
+  if option ~= nil then
+    return option
+  end
+  return default
 end
 
 return M
