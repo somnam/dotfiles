fa4aeca792e90ceb64f53f7c2064f75001e6c1cb
Tomek Miodek <tomek.miodek@gmail.com>
2025-11-02 15:33:34 +0100

    Staged changes

---
+++ home/tomek/.config/bashrc.d/plugins/colorfgbg.sh
@@ -0,0 +1,20 @@
+
+guess-colorfgbg () {
+    if [ -n "${COLORFGBG-}" ]; then
+        return 0
+    fi
+
+    local prefix
+    local bg
+    stty -echo
+    echo -ne '\x1b]11;?\x07'
+    IFS=: read -t 0.1 -d $'\a' prefix bg
+    if [[ ${bg:0:1} =~ [01234567] ]]; then
+        export COLORFGBG='15;0'
+    else
+        export COLORFGBG='0;15'
+    fi
+    stty echo
+}
+
+guess-colorfgbg
