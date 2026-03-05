b753909fd5fc688ffa181293aaa7bb8a7f114591
Tomek Miodek <tomek.miodek@gmail.com>
2026-03-05 21:52:26 +0100

    Staged changes

--- home/tomek/.config/bashrc.d/plugins/fzf.sh
+++ home/tomek/.config/bashrc.d/plugins/fzf.sh
@@ -5,5 +5,19 @@ if [[ -d "$HOME/.fzf" ]]; then
 fi
 
 if command -v fzf > /dev/null; then
+    export FZF_DEFAULT_COMMAND="find . -type f"
+    if command -v rg > /dev/null; then
+        export FZF_DEFAULT_COMMAND="rg --files"
+    elif command -v fd > /dev/null; then
+        export FZF_DEFAULT_COMMAND="fd --type f"
+    fi
+
+    export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+    export FZF_ALT_C_COMMAND="find . -type d"
+    if command -v fd > /dev/null; then
+        export FZF_ALT_C_COMMAND="fd --type d"
+    fi
+
     eval "$(fzf --bash)"
 fi
